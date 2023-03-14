bf753d5655a0956d1a4f765cf8b45d3762b808ee
Ajay Singh <125455606+Ajay01103@users.noreply.github.com>
2023-03-14 16:26:00 +0530

    initial commit

--- client/script.js
+++ client/script.js
@@ -86,7 +86,7 @@ const handleSubmit = async (e) => {
     // messageDiv.innerHTML = "..."
     loader(messageDiv)
 
-    const response = await fetch('http://localhost:5000', {
+    const response = await fetch('https://codex-openai-8hio.onrender.com', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
